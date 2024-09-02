a07bf9826c8e2f69e3061d95bc0657432a501a80
catarinafoliveira <felixcata@gmail.com>
2024-09-02 18:40:48 +0100

    initial commit

---
+++ controllers/baseController.js
@@ -0,0 +1,20 @@
+const CarModel = require('../models/carModel');
+
+exports.delete = async function(req, res, model) {
+    const modelName = model.modelName; 
+    console.log(`DELETE: /api/${modelName.toLowerCase()}s : ${req.params.id}`);
+    try {
+        const person = await model.findById(req.params.id);
+        if (!person) {
+            return res.status(404).json({ message: `${modelName} not found` });
+        }
+        const carsWithOwner = await CarModel.find({ owner: req.params.id });
+        if (carsWithOwner.length > 0) {
+            return res.status(400).json({ error: `Cannot delete ${modelName.toLowerCase()} because they are the owner of one or more cars.` });
+        }
+        await person.deleteOne();
+        res.status(200).json({ message: `${modelName} deleted!` });
+    } catch (err) {
+        res.status(500).json({ error: `Error deleting ${modelName.toLowerCase()}`, details: err });
+    }
+};
---
+++ models/personModel.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const PersonSchema = new Schema({
+  name: String,
+  idCard: {
+    type: String, 
+    unique: true, 
+    match: [/^[0-9]{9}$/, 'idCard must be exactly 9 numerical characters'],
+    minlength: [9, 'idCard must be exactly 9 numerical characters'],
+    maxlength: [9, 'idCard must be exactly 9 numerical characters']
+  }
+});
+
+const Person = mongoose.model('Person', PersonSchema);
+module.exports = Person;
